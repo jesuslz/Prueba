37991dd31b7fd8eeee2541761784044383a9b663
jesuslz <jlopez@integssoft.com>
2018-08-09 12:11:01 -0500

    main modificado

--- class1.py
+++ class1.py
@@ -1,8 +1,8 @@
 class MyClass(object):
-    def __init__(self, a, b):
-        self.a = a 
-        self.b = b 
+    def __init__(self, ao, bo):
+        self.a = ao 
+        self.b = bo 
         self.suma(self.a, self.b)
     
-    def suma(self, a, b):
-        return a + b
+    def suma(self, x = ao, y = b):
+        return ao + bo
